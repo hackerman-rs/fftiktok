fcc1493c662a9e5238c2db071a58b185ac2f8740
Violet McKinney <vi@violet.wtf>
2022-09-05 20:28:08 -0700

    part 2

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+# fftiktok - tiktok video downloader
+# Copyright (C) 2022  Violet McKinney <fftiktok@viomck.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from quart import Quart, redirect, send_file
 import requests
 
